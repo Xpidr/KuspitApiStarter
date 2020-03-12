92f9b1a320146b14c9fb47ea0b49522033fc5887
gabrieltorresm <45295218+gabrieltorresm@users.noreply.github.com>
2020-03-12 10:10:31 -0600

    Se elimina evento
    Por motivos de compatibilidad del evento onbeforeunload de con Chrome, se elimina la llamada al mismo y se cambia su funcionalidad a la función que se ejecuta al consumir el servicio, antes de cerrar la ventana.

--- src/main/resources/static/js/webhook.js
+++ src/main/resources/static/js/webhook.js
@@ -2,24 +2,10 @@
  * Este archivo contiene el código javascript que se utilizará en el archivo webhook.html
  */
 
-//Se registra el evento que se ejecutará una vez que se cierre la ventana de forma automática
-window.onbeforeunload = function(){
-	
-    // Manda llamar la funcion en la ventana principal cuando se cierra la ventana
-    // Se maneja con setTimeout para solventar un problema de Chrome 
-    // que no realiza correctamente el onbeforeunload
-    setTimeout(function() {
-        //Método que se ejecutará antes de cerrar la sesión, ver método closedOauth en archivo 
-	//main.js
-        window.opener.closedOauth();
-    
-        //Cierra la ventana
-        self.close();
-    }, 0);
-};
-
-//Consume el endpoint para obtener el primer token del usuario
-//y cierra la ventana al terminar
+//Consume el endpoint para obtener el primer token del usuario,
+//manda llamar la funcion de la ventana de origen para mostrar los
+//datos del primer token y cierra la ventana al terminar
 $.get("/webhook/process",function(){
+	window.opener.closedOauth();
 	window.close();
-})
+});
